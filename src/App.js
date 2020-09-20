be875217b873f0edd3ae3f1f902a0aee9941e052
sushma.k <sushma.k@metricstream.com>
2020-09-20 20:19:01 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React,{ Component } from 'react';
+import { Fragment } from 'react';
 
 
 class App extends Component {
   render() {
       return (
-        <div>
-          Phone Directory
-        </div>
+        <Fragment>
+        
+            <div className="header">
+            Phone Directory
+            </div>
+            <label htmlFor="name">Name:</label>
+            <input id="name" type="text" placeholder="Your Name"  defaultValue="Sushma" />
+            <button>Add</button>
+            <div>
+              <span>Name</span><br/>
+              <span>Phone</span>
+            </div>
+        
+        </Fragment>
       );
     }
   }
